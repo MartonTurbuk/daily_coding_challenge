a3d14d67209540f48494bffbc7c258a607649a2b
Turbuk Márton <turbuk.marton@nwmgroup.hu>
2019-02-22 22:21:26 +0100

    Daily coding problem #2.

---
+++ 2019/February/02_21.py
@@ -0,0 +1,15 @@
+numbers = [10, 15, 3, 7]
+
+k = 17
+
+
+def two_sum(lst, number):
+    seen = set()
+    for num in lst:
+        if number - num in seen:
+            return True
+        seen.add(num)
+    return False
+
+
+print(two_sum(numbers, k))
--- 2019/February/02_22.py
+++ 2019/February/02_22.py
@@ -1,15 +1,19 @@
-numbers = [10, 15, 3, 7]
+from functools import reduce
+# This problem was asked by Uber.
+#
+# Given an array of integers, return a new array such that each element
+# at index i of the new array is the product of all the numbers in the original array except the one at i.
+#
+# For example, if our input was [1, 2, 3, 4, 5], the expected output would be
+# [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
 
-k = 17
+numList = [1, 2, 3, 4, 5]
+resultList = []
 
+for number in numList:
+    tempList = numList.copy()
+    tempList.remove(number)
+    resultList.append(reduce(lambda x, y: x * y, tempList))
 
-def two_sum(lst, number):
-    seen = set()
-    for num in lst:
-        if number - num in seen:
-            return True
-        seen.add(num)
-    return False
+print(resultList)
 
-
-print(two_sum(numbers, k))
