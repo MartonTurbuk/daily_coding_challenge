f8f45d1b71fd446ae1b6455fa3d9e3b1f560a016
Turbuk Márton <turbuk.marton@nwmgroup.hu>
2019-03-05 17:25:51 +0100

    Daily coding challange, plus solutions, and I added the next task as well.

--- 2019/March/03_02.py
+++ 2019/March/03_02.py
@@ -8,6 +8,7 @@
 # Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
 
 # My own solutions
+
 query = 'de'
 
 strings = ['dog', 'deer', 'deal']
@@ -29,3 +30,64 @@ def auto_complete_2(q, string_list):
 print(auto_complete(query, strings))
 
 print(auto_complete_2(query, strings))
+
+# DailyCoding solutions:
+
+words_1 = ['foo', 'bar', ...]
+
+
+def autocomplete(s):
+    results = set()
+    for word in words_1:
+        if word.startswith(s):
+            results.add(word)
+    return results
+
+
+ENDS_HERE = '__ENDS_HERE'
+
+words = ['foo', 'bar']
+
+
+class Trie(object):
+    def __init__(self):
+        self._trie = {}
+
+    def insert(self, text):
+        trie = self._trie
+        for char in text:
+            if char not in trie:
+                trie[char] = {}
+            trie = trie[char]
+        trie[ENDS_HERE] = True
+
+    def elements(self, prefix):
+        d = self._trie
+        for char in prefix:
+            if char in d:
+                d = d[char]
+            else:
+                return []
+        return self._elements(d)
+
+    def _elements(self, d):
+        result = []
+        for c, v in d.items():
+            if c == ENDS_HERE:
+                subresult = ['']
+            else:
+                subresult = [c + s for s in self._elements(v)]
+            result.extend(subresult)
+        return result
+
+
+trie = Trie()
+for word in words:
+    trie.insert(word)
+
+
+def autocomplete(s):
+    suffixes = trie.elements(s)
+    return [s + w for w in suffixes]
+
+
