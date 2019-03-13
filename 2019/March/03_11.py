5e1ffac6659a2e59e2baa48a5919f3fca4e59db6
Turbuk Márton <turbuk.marton@nwmgroup.hu>
2019-03-13 17:04:07 +0100

    Daily coding challange, plus solutions, and I added the next task as well.

--- 2019/March/03_11.py
+++ 2019/March/03_11.py
@@ -7,3 +7,26 @@
 # In this example, assume nodes with the same value are the exact same node objects.
 #
 # Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.
+
+
+def length(head):
+	if not head:
+		return 0
+	return 1 + length(head.next)
+
+
+def intersection(a, b):
+	m, n = length(a), length(b)
+	cur_a, cur_b = a, b
+
+	if m > n:
+		for _ in range(m - n):
+			cur_a = cur_a.next
+	else:
+		for _ in range(n - m):
+			cur_b = cur_b.next
+
+	while cur_a != cur_b:
+		cur_a = cur_a.next
+		cur_b = cur_b.next
+	return cur_a
